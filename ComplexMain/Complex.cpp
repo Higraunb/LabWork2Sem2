6d8de4d558db31825deb2bcc685bc5132d3f21e9
higraunb <bear.porf@gmail.com>
2025-03-14 09:23:27 +0300

    new commit

--- ComplexMain/Complex.cpp
+++ ComplexMain/Complex.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
-	TComplex <int> a;
-	//cout << a;
+	TComplex <double> a(10, 3), d(5, 5), c;
+	a *= d;
+	cout << a;
+	a += d;
+	cout << a ;
+	a -= d;
+	cout << a;
+	a /= d;
+	cout << a;
+	c = d;
+	cout << c;
+	cout << (c == d) << endl;
+	c = a - d;
+	cout << c;
 	return 0;
 }
