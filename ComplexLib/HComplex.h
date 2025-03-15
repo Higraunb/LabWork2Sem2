93b3488c4d2512c5b293cfb21fb1e2a110121a1f
higraunb <bear.porf@gmail.com>
2025-03-15 11:15:01 +0300

    new commit

--- ComplexLib/HComplex.h
+++ ComplexLib/HComplex.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <complex.h>
 using namespace std;
 template <class T>
 class TComplex
@@ -13,6 +14,8 @@ public:
 	T GetIm();
 	void SetRe(T val);
 	void SetIm(T val);
+	T ComplexModule();
+	T ComplexDegree();
 	TComplex operator + (const TComplex<T>& val);
 	TComplex operator - (const TComplex<T>& val);
 	TComplex operator * (const TComplex<T>& val);
@@ -33,3 +36,165 @@ public:
 private:
 	T real, imaginary;
 };
+
+template <class T>
+inline TComplex<T>::TComplex(): real(1), imaginary(0)
+{}
+
+template <class T>
+inline TComplex<T>::TComplex(T real_, T imaginary_) : real(real_), imaginary(imaginary_)
+{}
+
+template <class T>
+inline TComplex<T>::~TComplex()
+{}
+
+template <class T>
+inline TComplex<T>::TComplex(TComplex& val)
+{
+	real = val.GetRe();
+	imaginary = val.GetIm();
+}
+
+template <class T>
+inline T TComplex<T>::GetRe()
+{
+	return real;
+}
+
+template <class T>
+inline T TComplex<T>::GetIm()
+{
+	return imaginary;
+}
+
+template <class T>
+inline void TComplex<T>::SetRe(T val)
+{
+	real = val;
+}
+
+template <class T>
+inline void TComplex<T>::SetIm(T val)
+{
+	im = val;
+}
+
+template<class T>
+inline T TComplex<T>::ComplexModule()
+{
+	return sqrt(real*real + imaginary*imaginary);
+}
+
+//template<class T>
+//inline T TComplex<T>::ComplexDegree()
+//{
+//	pow();
+//	return;
+//}
+
+template<class T>
+inline TComplex<T> TComplex<T>::operator + (const TComplex<T>& val)
+{
+	return TComplex(real + val.real, imaginary + val.imaginary);
+}
+
+template<class T>
+inline TComplex<T> TComplex<T>::operator - (const TComplex<T>& val)
+{
+	return TComplex(real - val.real, imaginary - val.imaginary);
+}
+
+template<class T>
+inline TComplex<T> TComplex<T>::operator * (const TComplex<T>& val)
+{
+	return TComplex(real * val.real - imaginary * val.imaginary,
+		real * val.imaginary + imaginary * val.real);
+}
+
+template<class T>
+inline TComplex<T> TComplex<T>::operator / (const TComplex<T>& val)
+{
+	return TComplex((real * val.real + imaginary * val.imaginary)
+		/ (val.real * val.real + val.imaginary * val.imaginary),
+		(real * val.imaginary + imaginary * val.real)
+		/ (val.real * val.real + val.imaginary * val.imaginary));
+}
+
+template<class T>
+inline TComplex<T>& TComplex<T>::operator+=(const TComplex<T>& val)
+{
+	real += val.real;
+	imaginary += val.imaginary;
+	return*this;
+}
+
+template<class T>
+inline TComplex<T>& TComplex<T>::operator -= (const TComplex<T>& val)
+{
+	real -= val.real;
+	imaginary -= val.imaginary;
+	return *this;
+}
+
+template<class T>
+inline TComplex<T>& TComplex<T>::operator *= (const TComplex<T>& val)
+{
+	T re = real;
+	T im = imaginary;
+	real = re * val.real - im * val.imaginary;
+	imaginary = re * val.imaginary + im * val.real;
+	return *this;
+}
+
+template<class T>
+inline TComplex<T>& TComplex<T>::operator /= (const TComplex<T>& val)
+{
+	T re = real;
+	T im = imaginary;
+	real = ((re * val.real) + (im * val.imaginary)) / ((val.real * val.real) + (val.imaginary * val.imaginary));
+	imaginary = ((im * val.real) - (re * val.imaginary)) / ((val.real * val.real) + (val.imaginary * val.imaginary));
+	return *this;
+}
+
+template<class T>
+inline TComplex<T>& TComplex<T>::operator = (const TComplex<T>& val)
+{
+	real = val.real;
+	imaginary = val.imaginary;
+	return*this;
+}
+
+template<class T>
+inline bool TComplex<T>::operator == (const TComplex<T>& val)
+{
+	return ((this->real == val.real) && (this->imaginary == val.imaginary));
+}
+
+template<class T>
+inline bool TComplex<T>::operator != (const TComplex<T>& val)
+{
+	return ((this->real != val.real) || (this->imaginary != val.imaginary));
+}
+
+
+template <class T>
+inline ostream& operator << (ostream& out, TComplex<T>& val)
+{
+	out << "Complex number - " << val.GetRe() << " + " << val.GetIm() << " * i " << endl;
+	system("pause");
+	system("cls");
+	return out;
+}
+
+template <class T>
+inline istream& operator >> (istream& inp, TComplex<T>& val)
+{
+	cout << "Enter real - ";
+	inp >> val.re;
+	cout << "Enter imaginary - ";
+	inp >> val.im;
+	system("pause");
+	system("cls");
+	return inp;
+}
